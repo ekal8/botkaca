a3d44a255cb474081a6cfc6af1760fb4e0782ead
ekal8 <75772252+ekal8@users.noreply.github.com>
2021-05-27 17:48:34 +0800

    Update en.py

--- bot/locals/en.py
+++ bot/locals/en.py
@@ -8,7 +8,7 @@ LOCAL = Local({
     'LEECH_LIST_MESSAGE_HEADER' : '<b>Leech Status</b>',
     'LEECH_LIST_FORMAT' : 'Name: <code>{name}</code>\nStatus: {status}\nID: <code>{gid}</code>\n\n',
     'ARIA2_CHECKING_LINK' : "Checking...",
-    'ARIA2_DOWNLOAD_STATUS' : "Downloading : <code>{name}</code>\n[{block}] {percentage}\nSize : {total_size}\n{current} Of {total_size}\n📥 : {download_speed} / 📤 : {upload_speed}\nSeeder : {seeder}\nETA : {eta}",
+    'ARIA2_DOWNLOAD_STATUS' : "Downloading : <code>{name}</code>\n[{block}] {percentage}\nSize : {total_size}\n📥 : {download_speed} / 📤 : {upload_speed}\nSeeder : {seeder}\nETA : {eta}",
     'ARIA2_DOWNLOAD_SUCCESS' : 'File downloaded : <code>{name}</code>',
     'ARIA2_DOWNLOAD_CANCELED' : 'Download canceled : <code>{name}</code>',
     'ARIA2_DEAD_LINK' : 'Download auto canceled : <code>{name}</code>\nYour Torrent/Link is Dead.',
